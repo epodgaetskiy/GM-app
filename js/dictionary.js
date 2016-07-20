4489ff4ab9bac715c3b7725db32012a28c86a90f
Evgeniy Podgaetskiy <epodgaetskiy@gmail.com>
2016-07-20 09:18:59 +0300

    production

---
+++ js/dictionary.js
@@ -0,0 +1,63 @@
+var itemNumber = 0;
+
+function searchCheckboxToggleState() {
+	/*$(this).find('svg').toggle(100, function() {
+		$(this).find('.checkbox-icon').hide();
+		$(this).find('.check-icon-checked').show();
+	});*/
+	$('.word-checkbox').prop('checked', true);
+	$('.main_word').addClass('main_word-choosed');
+	searchMenuChangeState();
+}
+
+function selectWordCheckboxToggle(){
+	$(this).parents('.main_word').find('.word-checkbox').prop('checked', !$(this).siblings('.word-checkbox').prop('checked'))
+	$(this).parents('.main_word').toggleClass('main_word-choosed');
+	searchMenuChangeState();
+}
+
+function searchMenuChangeState() {
+	itemNumber = $('.word-checkbox:checked').length;
+	if(itemNumber >= 1) {
+		$('.main_search').css('display', 'none');
+		$('.main_addNewWord_panel').css('display', 'flex');
+		$('.addNewWord_panel-active-count').html(itemNumber);
+	} else {
+		$('.main_search').css('display', 'flex');
+		$('.main_addNewWord_panel').css('display', 'none');
+		$('.addNewWord_panel-active-count').html('0');
+	}
+}
+function uncheckAllAtAddWordPanel() {
+	$('.word-checkbox').prop('checked', false);
+	$('.main_word').removeClass('main_word-choosed');
+	searchMenuChangeState()
+}
+
+$(document).ready(function(){
+	$('.main_search_checkbox').on('click', searchCheckboxToggleState);
+	$('.word-checkbox-icon').on('click', selectWordCheckboxToggle);
+	$('.addNewWord_panel-active > svg').on('click', uncheckAllAtAddWordPanel)
+
+
+	$('.language_selector').click(function() {
+		$(this).toggleClass('active');
+		$(this).siblings('.languages').slideToggle(1);
+	});
+
+	$('.navbar_language-select-title').on('click', function(){
+		$('.navbar_languages-select-list').toggle(1);
+		$('.navbar_languages-select-list').css('display','flex');
+	})
+	$('.library_selectCategory').on('click', function() {
+		$('.library_categories-select-list').toggleClass('active');
+	})
+
+	$('.library-card-addButton')
+	.mouseenter(function(){
+		$(this).find('.library-card-tooltip').delay(500).css('display', 'flex');
+	})
+	.mouseleave(function() {
+		$(this).find('.library-card-tooltip').css('display', 'none');
+	})	
+})
